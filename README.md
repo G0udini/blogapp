40cc45a338789e6bf73fa12cc207b9b3a2ddc250
G0udini <ruslan.shabunin2010@yandex.ru>
2021-08-24 16:50:46 +0300

    add comments for posts and form for publish new

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ simple blog app
 3. use date and slug to have pretty urls
 4. paginations for published posts
 5. configure static files with html
+6. email sending form
+7. comments for every blog post
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+
 
 urlpatterns = [
     path("blog/", include("blog_app.urls", namespace="blog")),
     path("admin/", admin.site.urls),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
