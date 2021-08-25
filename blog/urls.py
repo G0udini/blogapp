89b111a8c196db4c7c28a8a56e4f336079ba0bc0
G0udini <ruslan.shabunin2010@yandex.ru>
2021-08-25 14:21:49 +0300

    add sitemap and rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.contrib.sitemaps.views import sitemap
+from blog_app.sitemaps import PostSitemap
 
+sitemaps = {
+    "posts": PostSitemap,
+}
 
 urlpatterns = [
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
     path("blog/", include("blog_app.urls", namespace="blog")),
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,12 @@
+from blog_app.feeds import LatestPostsFeed
 from django.urls import path
 from .views import post_detail, post_list, post_share
+from .feeds import LatestPostsFeed
 
 app_name = "blog_app"
 urlpatterns = [
-    path('tag/<slug:tag_slug>', post_list, name="post_list_by_tag"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
+    path("tag/<slug:tag_slug>", post_list, name="post_list_by_tag"),
     path("", post_list, name="post_list"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
