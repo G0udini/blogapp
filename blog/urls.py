0ea74e9bcea3e507f37ba75cfcc7e3dd57aa14d5
G0udini <ruslan.shabunin2010@yandex.ru>
2021-12-14 00:43:31 +0300

    add media files

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 import debug_toolbar
 from django.contrib.sitemaps.views import sitemap
 from blog_app.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 sitemaps = {
     "posts": PostSitemap,
@@ -19,3 +21,5 @@ urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("", include("blog_app.urls", namespace="blog")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
